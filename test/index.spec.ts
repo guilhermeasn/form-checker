5ffca866b413a325075c4a5c0146fca0da197d15
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2025-04-18 19:53:24 -0300

    example test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -94,4 +94,29 @@ describe('formChecker', () => {
         expect(result.mensages.email).toBe('Email is invalid');
     });
 
+    test('test readme example', () => {
+
+        type FormFields = 'name' | 'email' | 'password' | 'password_confirm';
+
+        const schema : FormCheckerSchema<FormFields> = {
+            name: { required: true, minLength: 3, maxLength: 30 },
+            email: { required: true, minLength: 5, maxLength: 50 },
+            password: { required: true, minLength: 6, maxLength: 20, regexp: [/[a-z]/, /[A-Z]/, /[0-9]/] },
+            password_confirm: { required: true, equal: 'password' }
+        };
+
+        const data : Record<FormFields, string> = {
+            name: 'Guilherme',
+            email: 'contato@gn.dev.br',
+            password: 'Q1w2E3r4',
+            password_confirm: 'Q1w2E3r4'
+        };
+
+        formChecker(schema, data).then(result => {
+            console.log(result);
+            expect(result.isValid).toBe(true);
+        });
+
+    })
+
 });
