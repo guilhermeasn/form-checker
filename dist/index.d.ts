1148b73de307a9630e19e7dbc931a1daf77fdaca
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2025-04-24 20:16:00 -0300

    export all types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import type { FormCheckerData, FormCheckerDefaultMessages, FormCheckerError, FormCheckerLanguages, FormCheckerResult, FormCheckerRules, FormCheckerSchema, FormCheckerValue } from "./types";
+import type { FormCheckerData, FormCheckerDefaultMessages, FormCheckerError, FormCheckerLanguages, FormCheckerResult, FormCheckerRuleRequired, FormCheckerRules, FormCheckerSchema, FormCheckerValue } from "./types";
 /**
  * Form data validator
  */
 export declare function formChecker<Data extends FormCheckerData, Schema extends FormCheckerSchema<Data>>(schema: Schema, data: Data, language?: FormCheckerDefaultMessages | FormCheckerLanguages): Promise<FormCheckerResult<Data>>;
-export type { FormCheckerData, FormCheckerDefaultMessages, FormCheckerError, FormCheckerLanguages, FormCheckerResult, FormCheckerRules, FormCheckerSchema, FormCheckerValue };
+export type { FormCheckerData, FormCheckerDefaultMessages, FormCheckerError, FormCheckerLanguages, FormCheckerResult, FormCheckerRuleRequired, FormCheckerRules, FormCheckerSchema, FormCheckerValue };
 export default formChecker;
